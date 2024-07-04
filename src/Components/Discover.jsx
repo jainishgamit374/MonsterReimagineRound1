98e65dadcf8ef5604f4d537da8d2ff83b2709933
xvsanuj <anuj.shar2k8@gmail.com>
2024-07-04 21:28:54 +0530

    Discover Page Fixed

--- src/Components/Discover.jsx
+++ src/Components/Discover.jsx
@@ -8,15 +8,11 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Discover = () => {
     const scrollRef = useRef(null);
-
     useGSAP(() => {
       const img = scrollRef.current.querySelector('.can-img');
-  
       const createScrollTrigger = () => {
         const mq = window.matchMedia("(max-width: 768px)"); // md breakpoint
-  
         ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-  
         gsap.to(img, {
           y: mq.matches ? "-400" : "-50",
           x: mq.matches ? "-100" : "10",
@@ -52,16 +48,13 @@ const Discover = () => {
           y: -200,
         }, 'i');
       };
-  
       createScrollTrigger();
       window.addEventListener('resize', createScrollTrigger);
-  
       return () => {
         window.removeEventListener('resize', createScrollTrigger);
         ScrollTrigger.getAll().forEach(trigger => trigger.kill());
       };
     }, []);
-
     return (
         <div className="w-full relative">
         <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl tracking-tighter text-center pt-20 monument">
@@ -70,22 +63,24 @@ const Discover = () => {
         <p className="text-center text-xs sm:text-sm md:text-base monument pb-10">
           The world of Red Bull
         </p>
-        <div className="max-w-screen-xl m-auto min-h-screen relative py-8">
-          <div className="img-container w-full h-full grid grid-cols-2 md:grid-cols-3 gap-4 p-4 justify-center items-center">
-            <div className="drop-shadow-2xl flex items-center justify-center rounded-xl  md:rounded-3xl border-1 md:border-2 bg-[#060B27] text-white">
-              <h1 className="text-[2vw] sm:text-sm py-4 md:py-8">⭐ Dive In To all Flavours</h1>
+        <div className="h-[80vh] w-full flex items-center justify-evenly">
+          <div className="firstDiv flex flex-col gap-8">
+            <div className="h-20 flex items-center justify-center text-white tracking-tighter w-96 rounded-2xl bg-[#060B27]">
+              <h1>⭐ Dive In To all Flavours</h1>
             </div>
-            <div className="flex items-center justify-center  row-span-2 md:row-span-3 rounded-3xl md:rounded-3xl border-2 text-white z-20" ref={scrollRef}>
-              <img className="can-img h-[50vw] md:h-[30vw] object-contain" src="https://m.media-amazon.com/images/I/51JFRoM+7HL._AC_UF1000,1000_QL80_.jpg" alt="" />
+            <div>
+                <img className="h-96 rounded-2xl w-96 object-cover" src="https://img.redbull.com/images/c_fill,f_auto,q_auto,w_770,h_578,g_auto,g_auto/redbullcom/2020/5/22/mey8s5v0xtjdm3v9vmul/the-red-bulletin-logo" alt="" />
             </div>
-            <div className="drop-shadow-2xl flex items-center justify-center row-span-1 md:row-span-2 rounded-lg md:rounded-3xl overflow-hidden z-10">
-              <img className="right-img rounded-3xl w-full h-full object-top object-cover" src="https://i.pinimg.com/564x/53/d0/36/53d036121daaf0aba535bc22b082dfee.jpg" alt="" />
+          </div>
+          <div className="secondDiv">
+            <div ref={scrollRef} className="h-[70vh] rounded-2xl border-2 flex items-center justify-center w-96 bg-white">
+              <img className="can-img h-96 object-contain" src="https://m.media-amazon.com/images/I/51JFRoM+7HL._AC_UF1000,1000_QL80_.jpg" alt="" />
             </div>
-            <div className="drop-shadow-2xl flex items-center justify-center  md:row-span-2 rounded-lg md:rounded-3xl overflow-hidden z-10">
-              <img className="left-img rounded-3xl w-full h-full object-cover" src="https://img.redbull.com/images/c_fill,f_auto,q_auto,w_770,h_578,g_auto,g_auto/redbullcom/2020/5/22/mey8s5v0xtjdm3v9vmul/the-red-bulletin-logo" alt="" />
-            </div>
-            <div className="drop-shadow-2xl flex items-center justify-center rounded-xl md:rounded-3xl border-1 md:border-2 bg-[#060B27] text-zinc-300 py-4 md:py-8">
-              <a className="text-[2vw] sm:text-sm  md:py-8 flex items-center gap-2  md:gap-4 cursor-pointer">Over New Products <MdArrowOutward /></a>
+          </div>
+          <div className="thirdDiv flex flex-col gap-8">
+            <img className="h-96 rounded-2xl w-96 object-cover object-top" src="https://i.pinimg.com/564x/53/d0/36/53d036121daaf0aba535bc22b082dfee.jpg" alt="" />
+            <div className="h-20 flex items-center justify-center w-96 bg-[#060B27] rounded-2xl">
+            <h1 className="text-white flex items-center gap-2">Our New Products <MdArrowOutward /></h1>
             </div>
           </div>
         </div>
