80941d36dc28cdaaf6d129e08a60f222d7bae798
xvsanuj <anuj.shar2k8@gmail.com>
2024-07-05 23:17:43 +0530

    Responsive Commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,13 @@
 import gsap from "gsap";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
 
 const Home = () => {
+  const scrollRef = useRef(null);
   useGSAP(() => {
-
     var t1 = gsap.timeline();
-
     t1.from(".animeHeading span", {
       y: 200,
       stagger: 0.1,
@@ -17,13 +16,14 @@ const Home = () => {
       opacity: 0,
     }, "h");
 
-            t1.from(".secondtext span", {
-              y: 200,
-              stagger: 0.1,
-              duration: 2,
-              ease: "power2.inOut",
-              opacity: 0,
-            }, "h");
+    t1.from(".secondtext span", {
+      y: 200,
+      stagger: 0.1,
+      duration: 2,
+      ease: "power2.inOut",
+      opacity: 0,
+    }, "h");
+
 
     t1.from(".hero-images", {
       y: 200,
@@ -31,17 +31,9 @@ const Home = () => {
       duration: 2,
       ease: "power2.inOut",
       opacity: 0,
-    });
+    }, "h");
 
-            t1.from(".hero-images .side-img1 img", {
-              left: "50%",
-              rotate: "0deg",
-              stagger: 0.1,
-              duration: 1,
-              ease: "power2.inOut"
-            }, "p");
-
-    t1.from(".hero-images .side-img2 img", {
+    t1.from(".hero-images .side-img1 img", {
       left: "50%",
       rotate: "0deg",
       stagger: 0.1,
@@ -49,40 +41,46 @@ const Home = () => {
       ease: "power2.inOut"
     }, "p");
 
-
-
-
+    t1.from(".hero-images .side-img2 img", {
+      left: "50%",
+      rotate: "0deg",
+      stagger: 0.1,
+      duration: 1,
+      ease: "power2.inOut",
+    }, "p");
   });
   return (
     <>
       <div className="w-full h-screen overflow-hidden relative">
-        <h1 data-content="REDBULL" className="animeHeading flex font-[Kajiro] tracking-[5vw] text-[20vw] absolute  top-1/2 left-[52%] -translate-x-2/4 -translate-y-2/4">
-          <span className="inline-block font-[Kajiro] kajiro">R</span>
-          <span className="inline-block font-[Kajiro] kajiro">E</span>
-          <span className="inline-block font-[Kajiro] kajiro">D</span>
-        </div>
-        <div className="flex">
-        <span className="inline-block font-[Kajiro] kajiro">B</span>
-          <span className="inline-block font-[Kajiro] kajiro">U</span>
-          <span className="inline-block font-[Kajiro] kajiro">L</span>
-          <span className="inline-block font-[Kajiro] kajiro">L</span>
-        </div>
-        </div>
+        <h1 data-content="REDBULL" className="animeHeading flex font-[Kajiro] leading-none tracking-[5vw] text-[70vw] md:text-[20vw] absolute  top-1/2 md:top-[58%] left-[52%] -translate-x-2/4 -translate-y-2/4 z-[4]">
+          <div className="lg:flex">
+            <div className="flex items-center justify-center">
+              <span className="inline-block font-[Kajiro] kajiro">R</span>
+              <span className="inline-block font-[Kajiro] kajiro">E</span>
+              <span className="inline-block font-[Kajiro] kajiro">D</span>
+            </div>
+            <div className="flex items-center justify-center">
+              <span className="inline-block font-[Kajiro] kajiro">B</span>
+              <span className="inline-block font-[Kajiro] kajiro">U</span>
+              <span className="inline-block font-[Kajiro] kajiro">L</span>
+              <span className="inline-block font-[Kajiro] kajiro">L</span>
+            </div>
+          </div>
         </h1>
         <h1 data-content="REDBULL" className="secondtext flex font-[Kajiro] leading-none tracking-[5vw] text-transparent text-[70vw] md:text-[20vw] absolute z-[9]  top-1/2 md:top-[58%] left-[52%] -translate-x-2/4 -translate-y-2/4 ">
-        <div className="lg:flex">
-        <div className="flex">
-        <span className="inline-block font-[Kajiro] kajiro">R</span>
-          <span className="inline-block font-[Kajiro] kajiro">E</span>
-          <span className="inline-block font-[Kajiro] kajiro">D</span>
-        </div>
-        <div className="flex">
-        <span className="inline-block font-[Kajiro] kajiro">B</span>
-          <span className="inline-block font-[Kajiro] kajiro">U</span>
-          <span className="inline-block font-[Kajiro] kajiro">L</span>
-          <span className="inline-block font-[Kajiro] kajiro">L</span>
-        </div>
-        </div>
+          <div className="lg:flex">
+            <div className="flex items-center justify-center">
+              <span className="inline-block font-[Kajiro] kajiro">R</span>
+              <span className="inline-block font-[Kajiro] kajiro">E</span>
+              <span className="inline-block font-[Kajiro] kajiro">D</span>
+            </div>
+            <div className="flex items-center justify-center">
+              <span className="inline-block font-[Kajiro] kajiro">B</span>
+              <span className="inline-block font-[Kajiro] kajiro">U</span>
+              <span className="inline-block font-[Kajiro] kajiro">L</span>
+              <span className="inline-block font-[Kajiro] kajiro">L</span>
+            </div>
+          </div>
         </h1>
         <div className="hero-images w-full h-full absolute z-[8]">
           <div className="side-img1" ref={scrollRef}>
